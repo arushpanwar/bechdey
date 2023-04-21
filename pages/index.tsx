04e2bba7a1e5e29e8907dd50c98dbd50d02af42d
Arush Panwar <lcs2020032@iiitl.ac.in>
2023-04-21 17:32:55 +0530

    header and footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
+    <>
+    <Header />
     <h1 className="text-3xl font-bold bg-red-500">
       Hello world!
     </h1>
+    <Footer />
+    </>
   )
 }
