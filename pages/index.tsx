062dc24aad438bd052cdfa0ea08658d521f51406
chinmay <lcs2020022@iiitl.ac.in>
2023-04-22 19:02:34 +0530

    product compenent and routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Categories from "@/components/Categories";
+import ProductsList from "@/components/ProductsList";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
     <Header />
     <img src="/banner.jpg" alt="Banner Image" className="mx-auto w-full" />
     <Categories />
+    <ProductsList />
     <Footer />
     </>
   )
