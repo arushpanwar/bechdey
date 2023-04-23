7a5ebd21cb21a0dea9f1cbaf31ad65f7663d75e6
Arush Panwar <lcs2020032@iiitl.ac.in>
2023-04-24 01:02:28 +0530

    signup and login page

---
+++ pages/signup.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { supabase } from "../lib/supabase";
+
+export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  async function handleSignup() {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) {
+      setError(error.message);
+    } else {
+      router.push("/login");
+    }
+  }
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      {error && <p>{error}</p>}
+      <button onClick={handleSignup}>Sign Up</button>
+    </div>
+  );
+}
