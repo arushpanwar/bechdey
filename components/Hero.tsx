77d1364bf196d3033c4fea60387c0c2b9560f7e4
Arush Panwar <lcs2020032@iiitl.ac.in>
2023-04-24 22:55:07 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
     <div className=" relative flex justify-center">
-      <img src="/banner1.png" alt="banner1" className="banner px-20  h-[500px] " />
-      <p class="absolute inset-0 flex items-center justify-center text-center text-4xl font-semibold  text-white">Traditions Preserved</p>
+      <img
+        src="/banner1.png"
+        alt="banner1"
+        className="banner px-20  h-[500px] "
+      />
+      <div className="absolute inset-0 top-10 flex items-center justify-center text-center ">
+        <div className="flex flex-col">
+          <span className="text-4xl font-semibold mb-1  text-white">
+            Traditions Preserved
+          </span>
+          <span className="text-xl font-medium text-neutral-300">
+            Pices Reasonable
+          </span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
