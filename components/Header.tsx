7e36064c6aa84d63963a5eeca9f977875362714b
Arush Panwar <lcs2020032@iiitl.ac.in>
2023-04-25 02:17:27 +0530

    some design elements

--- components/Header.tsx
+++ components/Header.tsx
@@ -39,30 +39,67 @@ const Header = () => {
 
   return (
     <header className="text-gray-600 body-font flex flex-col justify-center">
-      <div className="container flex flex-wrap py-4 px-8 flex-col items-center ">
-        <div className="w-full flex justify-between items-center px-60">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7    h-7">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-        </svg>
+      <div className="flex py-4 px-8 flex-col items-center ">
+        <div className="w-full flex justify-between items-center px-[26rem]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-7    h-7"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
 
           <Link legacyBehavior href={"/"}>
             <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 py-2">
-              <img src="/logo.png" className="w-12 h-12 -rotate-45" alt="logo" />
-              <span className="ml-3 font-semibold text-blue-950 text-3xl">Rentway</span>
+              <img
+                src="/logo.png"
+                className="w-12 h-12 -rotate-45"
+                alt="logo"
+              />
+              <span className="ml-3 font-semibold text-blue-950 text-3xl">
+                Rentway
+              </span>
             </a>
           </Link>
           <div className="flex flex-row">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 mx-2">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7 mx-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
             </svg>
 
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7 mx-2 cursor-pointer " onClick={signInWithGoogle}>
-              
-              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-          </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7 mx-2 cursor-pointer "
+              onClick={signInWithGoogle}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              />
+            </svg>
           </div>
-          
-
         </div>
         <nav className="md:mx-auto flex flex-row justify-center items-center mb-2">
           <Link legacyBehavior href={"/products"}>
--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Testimonial = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white">
       <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-6">
         <figure className="max-w-screen-md mx-auto">
           <svg
-            className="h-12 mx-auto mb-3 text-gray-400 dark:text-gray-600"
+            className="h-12 mx-auto mb-3 text-gray-400"
             viewBox="0 0 24 27"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -17,21 +17,14 @@ const Testimonial = () => {
             />
           </svg>
           <blockquote>
-            <p className="text-2xl font-medium text-gray-900 dark:text-white">
-              "Flowbite is just awesome. It contains tons of predesigned
-              components and pages starting from login screen to complex
-              dashboard. Perfect choice for your next SaaS application."
+            <p className="text-2xl font-medium text-gray-900">
+              "Brilliant app, I love it."
             </p>
           </blockquote>
           <figcaption className="flex items-center justify-center mt-6 space-x-3">
-            <img
-              className="w-6 h-6 rounded-full"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png"
-              alt="profile picture"
-            />
-            <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-              <div className="pr-3 font-medium text-gray-900 dark:text-white">
-                Micheal Gough
+            <div className="flex items-center divide-x-2 divide-gray-500">
+              <div className="pr-3 font-medium text-gray-900 ">
+                Arush Panwar
               </div>
             </div>
           </figcaption>
