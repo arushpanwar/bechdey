acbf0674d15cd5a9c9372c53fa466d7c4af3b3ef
Varchronos <lcs2020062@iiitl.ac.in>
2023-04-24 21:59:46 +0530

    updated image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className=" relative flex justify-center">
       <img src="/banner1.png" alt="banner1" className="banner px-20  h-[500px] " />
-      <p class="absolute inset-0 flex items-center justify-center text-center text-4xl font-semibold  text-white">Traditions Preserved</p>
+      <p classNamez="absolute inset-0 flex items-center justify-center text-center text-4xl font-semibold  text-white">Traditions Preserved</p>
     </div>
   )
 }
